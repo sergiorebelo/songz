0a3682b81f553378eb80d0c0e58068406f170ed3
Sergio Rebelo <sergio.rebelo@ctw.bmwgroup.com>
2025-08-24 19:11:54 +0100

    removed the external caal from the transactional method

--- src/main/java/com/sergiorebelo/songz/service/IngestService.java
+++ src/main/java/com/sergiorebelo/songz/service/IngestService.java
@@ -23,30 +23,31 @@ public class IngestService {
         this.lastfmClient = lastfmClient; this.playService = playService; this.trackService = trackService;
     }
 
-    @Transactional
     public int lastfmIngest(Instant since) {
         Long fromEpoch = since != null ? since.getEpochSecond() : null;
         RecentTracksResponse resp = lastfmClient.getRecentTracks(fromEpoch);
 
+        return processTracksResponse(resp);
+    }
+
+    @Transactional
+    protected int processTracksResponse(RecentTracksResponse resp) {
         int inserted = 0;
         for (TrackItem trackItem : resp.recenttracks().track()) {
-            Instant played = trackItem.playedAt();
-            if (played == null) continue;
-            Track track = trackService.findByTitleAndArtist(trackItem.title(), trackItem.artistName())
-                    .orElseGet(() -> trackService.createNewTrack(trackItem.artistName(), trackItem.title()));
-
-            if (playService.existsByTrackAndPlayedAt(track, played)) {
-                continue; // Skip if already recorded
-            }
-
-            playService.recordPlay(track, played, LASTFM);
-            inserted++;
-
+            if (insertPlayedTrack(trackItem)) inserted++;
         }
-
         return inserted;
     }
 
+    private boolean insertPlayedTrack(TrackItem trackItem) {
+        Instant played = trackItem.playedAt();
+        if (played == null) return false;
+        Track track = trackService.findByTitleAndArtist(trackItem.title(), trackItem.artistName())
+                .orElseGet(() -> trackService.createNewTrack(trackItem.artistName(), trackItem.title()));
+        if (!playService.existsByTrackAndPlayedAt(track, played))  return false;
+        playService.recordPlay(track, played, LASTFM);
+        return true;
+    }
 
     public Instant lastIngestAt(String source) {
         return playService.lastPlayedAt(source);
